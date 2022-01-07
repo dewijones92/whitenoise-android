4f4faa0bb7f6c827ec56ef899e9f0b8eb2f7d9d9
Dewi Jones <dewijones92@gmail.com>
2022-01-07 23:21:58 +0000

    more

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e;
 set -x;
+gradle_version="4.4"
 
 
 function docker_run {
 	set -x;
 	set -e;
 	docker run --rm -u gradle  -e ANDROID_HOME=$android_dir -v "$ANDROID_HOME":$android_dir -v gradle-cache:/home/gradle/.gradle\
-		-v "$PWD":/home/gradle/project -w /home/gradle/project gradle:4.4 $@
+		-v "$PWD":/home/gradle/project -w /home/gradle/project gradle:$gradle_version $@
 }
 
 export android_dir="/home/gradle/Android/Sdk"
@@ -18,4 +19,6 @@ find | \grep -E "apk$|aab$" | xargs -d"\n" -n1 -t rm -rf;
 
 docker_run "./gradlew assemble"
 
-docker_run jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore ./app/build/outputs/apk/release/app-release-unsigned.apk alias_name;
+apk_path="./app/build/outputs/apk/release/app-release-unsigned.apk"
+docker_run jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore $apk_path alias_name;
+cp $apk_path ./release.apk
